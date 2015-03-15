7e15ead15de5134e18f043f571d336c70bfbbb42
David Chua <david.cys@live.com>
2015-03-15 15:58:42 +0800

    add profile search and show actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,18 @@ class ProfilesController < ApplicationController
     profile_service = ProfileService.new
     @profiles = profile_service.list
   end
+
+  def show
+    profile_service = ProfileService.new
+    @profile = profile_service.get(params[:id])
+    render text: "Not found", status: 4040 if !@profile
+  end
+
+  def search
+    if params.has_key?(:query)
+      profile_service = ProfileService.new
+      @profiles = profile_service.search(params[:query])
+    end
+  end
 end
 
