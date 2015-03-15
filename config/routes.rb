a2d0f05f104ca9684efb8d311ca1aeec941e2df6
David Chua <david.cys@live.com>
2015-03-15 15:20:44 +0800

    add foundation and an index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Dapiweb::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :profiles
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'profiles#index'
